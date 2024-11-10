2df1a1d24a0d1ff8b12d8049548c3e334a30b87c
shaz078 <shazcyber1@outlook.com>
2024-11-10 07:59:21 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '710898551-XoLEabcya-NoQPHMPshc'
+TOKEN = '7037600242:AAFixEMa47GDh6Qm9AEkSUGb45IZ5IxTIBc'
 MONGO_URI = 'mongodb+srv://Soul:JYAuvlizhw7wqLOb@soul.tsga4.mongodb.net'
-FORWARD_CHANNEL_ID = -100
-CHANNEL_ID = -100
-error_channel_id = -100
+FORWARD_CHANNEL_ID = -1002243580836
+CHANNEL_ID = -1002243580836
+error_channel_id = -1002243580836
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
